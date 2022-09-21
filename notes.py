8a0ac2a79bf556dca9b287880f184d64000666f1
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-09-21 10:11:20 +0530

    initial commit

---
+++ notes.py
@@ -0,0 +1,15 @@
+# python - used in server-side (web development)
+# handle big data
+# rapid prototyping
+# the language works on many platforms
+# runs on interpreter system - executes line by line, just as it is written (so useful for prototyping)
+
+
+# procedural, oops, functional - all the ways are possible
+# Integrated Dev Env for python: PyCHarm, Netbeans, Eclipse, THonny - used when handling many python files
+
+
+# PYTHON SYNTAX:
+# readable more
+# \n instead of ; to define statements
+# indendation instead of { ... } to define scope
