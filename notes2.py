5bda2fa95c65f18f0fca2af980869d81f2cd41c7
vijaydevelops <vijaynetdevaiml@gmail.com>
2022-09-21 11:51:56 +0530

    Progress 2022-09-21 FN

---
+++ notes2.py
@@ -0,0 +1,38 @@
+# python --version
+
+# to interpret python py file with an interpreter
+# python filename.py
+
+# single-line commands can be executed in python terminal, which is opened using commmand:
+# python
+
+# exit() to close the python cmd line
+
+# # used for commenting
+""" used for multiline docstrings
+test"""
+
+
+# no declaration syntax for variables in python
+#just
+x = 5
+y = "Paddle"
+print(x) #print function to output variable's values
+print(y)
+
+# note the variable data-type is not the same always
+x = "Happpy ?"
+print(x)
+
+#invalid varibale names: 1num
+#valid variable names: _num, num1, num_2
+# variable names - case sensitive
+
+
+# + used to add two numbers, join two strings, if different type variables are used with + sign, error shown
+
+z = 10
+
+print(y + x)
+print(1 + z)
+print(z + x)  # TypeError: unsupported operand type(s) for +: 'int' and 'str'
---
+++ notes3.py
@@ -0,0 +1,17 @@
+# values aka literals - number values types - below:
+# 3 types of numbers in python
+
+x = 1		# int
+y = 2.567	# float
+z = 3 + 5j	# complex
+z = 4j		# complex - j "imaginary part"
+
+y2 = 2.567E3
+y3 = 2.567e10
+
+print( type(x) )	# <class 'int'>
+print( type(y) )	# <class 'float'>
+print( type(z) )	# <class 'complex'>
+
+print( type(y2) )	# <class 'float'>
+print( type(y3) )	# <class 'float'> 
